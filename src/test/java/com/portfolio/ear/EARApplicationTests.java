ab4e19f71c377139d0c46f57e07d059b6d988b7f
AlejoPasquet <pedroalejopasquet@gmail.com>
2023-04-24 12:11:48 -0300

    proyecto de lizzy
    subida proyecto para realizar pruebas

---
+++ src/test/java/com/portfolio/ear/EARApplicationTests.java
@@ -0,0 +1,12 @@
+//package com.portfolio.ear;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class EARApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+//}
